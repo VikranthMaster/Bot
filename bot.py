564b2dc2c4b33d44d435f2e5a7340c4723d1396a
VikranthMaster <69100217+VikranthMaster@users.noreply.github.com>
2021-01-14 09:38:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands, timers
+from discord.utils import get
 from discord import DMChannel
 import os
 import asyncio
@@ -12,7 +13,9 @@ import requests
 import youtube_dl
 import aiohttp
 import json
-from weather import *
+import shutil
+from os import system
+
 
 player1 = ""
 player2 = ""
@@ -129,6 +132,30 @@ async def welcome(ctx, channel:discord.TextChannel):
     cursor.close()
     db.close()
 
+key_features = {
+    'temp' : "Temperature",
+    'feels_like' : "Feels Like",
+    'temp_min' : 'Minimum Temperature',
+    'temp_max' : 'Maximum Temperature',
+    'humidity' : 'Humidity',
+    'pressure' : 'Pressure' 
+}
+
+def parse_data(data):
+    data = data['main']
+    return data
+
+def weather_message(data, location):
+    location = location.title()
+    message = discord.Embed(title=f"{location} Weather", description=f"We have weather data for {location}", color = discord.Color.blue())
+    for key in data:
+        message.add_field(name=key_features[key], value=str(data[key]), inline=False)
+    return message
+
+def error_message(location):
+    location = location.title()
+    return discord.Embed(title="Error", description=f"There is no location like that or there was a error receving the data for {location}", color = discord.Color.blue())
+
 @client.event
 async def on_message(message):
   if message.content.find("Time to put down your phone and get back to work! Enter my room code") != -1:
@@ -145,6 +172,9 @@ async def on_message(message):
         await message.channel.send(embed=weather_message(data, location))
       except KeyError:
         await message.channel.send(embed=error_message(location))
+  remove = message.content.replace(">play", "").lower()
+  
+
 
   db = sqlite3.connect('test.sqlite') 
   cursor = db.cursor()
@@ -359,67 +389,248 @@ async def meme(ctx):
       await ctx.send(embed=embed)
 
 
-@client.command()
-async def play(ctx, url : str):
+@client.command(pass_context=True, aliases=['j', 'joi'])
+async def join(ctx):
+    global voice
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+
+    await voice.disconnect()
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+        print(f"The bot has connected to {channel}\n")
+
+    await ctx.send(f"Joined {channel}")
+
+
+@client.command(pass_context=True, aliases=['l', 'lea'])
+async def leave(ctx):
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f"The bot has left {channel}")
+        await ctx.send(f"Left {channel}")
+    else:
+        print("Bot was told to leave voice channel, but was not in one")
+        await ctx.send("Don't think I am in a voice channel")
+
+
+@client.command(pass_context=True, aliases=['p', 'pla'])
+async def play(ctx, url: str):
+
+    def check_queue():
+        Queue_infile = os.path.isdir("./Queue")
+        if Queue_infile is True:
+            DIR = os.path.abspath(os.path.realpath("Queue"))
+            length = len(os.listdir(DIR))
+            still_q = length - 1
+            try:
+                first_file = os.listdir(DIR)[0]
+            except:
+                print("No more queued song(s)\n")
+                queues.clear()
+                return
+            main_location = os.path.dirname(os.path.realpath(__file__))
+            song_path = os.path.abspath(os.path.realpath("Queue") + "\\" + first_file)
+            if length != 0:
+                print("Song done, playing next queued\n")
+                print(f"Songs still in queue: {still_q}")
+                song_there = os.path.isfile("song.mp3")
+                if song_there:
+                    os.remove("song.mp3")
+                shutil.move(song_path, main_location)
+                for file in os.listdir("./"):
+                    if file.endswith(".mp3"):
+                        os.rename(file, 'song.mp3')
+
+                voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
+                voice.source = discord.PCMVolumeTransformer(voice.source)
+
+            else:
+                queues.clear()
+                return
+
+        else:
+            queues.clear()
+            print("No songs were queued before the ending of the last song\n")
+
+
+
     song_there = os.path.isfile("song.mp3")
     try:
         if song_there:
             os.remove("song.mp3")
+            queues.clear()
+            print("Removed old song file")
     except PermissionError:
-        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        print("Trying to delete song file, but it's being played")
+        await ctx.send("ERROR: Music playing")
         return
 
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='General')
-    await voiceChannel.connect()
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+
+    Queue_infile = os.path.isdir("./Queue")
+    try:
+        Queue_folder = "./Queue"
+        if Queue_infile is True:
+            print("Removed old Queue Folder")
+            shutil.rmtree(Queue_folder)
+    except:
+        print("No old Queue folder")
+
+    await ctx.send("Getting everything ready now")
+
+    voice = get(client.voice_clients, guild=ctx.guild)
 
     ydl_opts = {
         'format': 'bestaudio/best',
+        'quiet': True,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
     }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+  
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+          print("Downloading audio now\n")
+          ydl.download([url])
+    except:
+      print("FALLBACK: youtube-dl does not support this URL, using Spotify (This is normal if spotify URL)")
+      c_path = os.path.dirname(os.path.realpath(__file__))
+
+      # remove = ctx.replace(">play","").lower()
+      # system(f"spotdl {remove}")
+      
+
+
     for file in os.listdir("./"):
         if file.endswith(".mp3"):
+            name = file
+            print(f"Renamed File: {file}\n")
             os.rename(file, "song.mp3")
-    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+    voice.play(discord.FFmpegPCMAudio("song.mp3"), after=lambda e: check_queue())
+    voice.source = discord.PCMVolumeTransformer(voice.source)
+
+    nname = name.rsplit("-", 2)
+    await ctx.send(f"Playing: {nname[0]}")
+    print("playing\n")
 
 
-@client.command()
-async def leave(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_connected():
-        await voice.disconnect()
-    else:
-        await ctx.send("The bot is not connected to a voice channel.")
-
-
-@client.command()
+@client.command(pass_context=True, aliases=['pa', 'pau'])
 async def pause(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_playing():
+
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_playing():
+        print("Music paused")
         voice.pause()
+        await ctx.send("Music paused")
     else:
-        await ctx.send("Currently no audio is playing.")
+        print("Music not playing failed pause")
+        await ctx.send("Music not playing failed pause")
 
 
-@client.command()
+@client.command(pass_context=True, aliases=['r', 'res'])
 async def resume(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_paused():
+
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_paused():
+        print("Resumed music")
         voice.resume()
+        await ctx.send("Resumed music")
     else:
-        await ctx.send("The audio is not paused.")
+        print("Music is not paused")
+        await ctx.send("Music is not paused")
 
 
-@client.command()
+@client.command(pass_context=True, aliases=['s', 'sto'])
 async def stop(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    voice.stop()
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    queues.clear()
+
+    queue_infile = os.path.isdir("./Queue")
+    if queue_infile is True:
+      shutil.rmtree("./Queue")
+
+    if voice and voice.is_playing():
+        print("Music stopped")
+        voice.stop()
+        await ctx.send("Music stopped")
+    else:
+        print("No music playing failed to stop")
+        await ctx.send("No music playing failed to stop")
+
+
+queues = {}
+
+@client.command(pass_context=True, aliases=['q', 'que'])
+async def queue(ctx, url: str):
+    Queue_infile = os.path.isdir("./Queue")
+    if Queue_infile is False:
+        os.mkdir("Queue")
+    DIR = os.path.abspath(os.path.realpath("Queue"))
+    q_num = len(os.listdir(DIR))
+    q_num += 1
+    add_queue = True
+    while add_queue:
+        if q_num in queues:
+            q_num += 1
+        else:
+            add_queue = False
+            queues[q_num] = q_num
+
+    queue_path = os.path.abspath(os.path.realpath("Queue") + f"\song{q_num}.%(ext)s")
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'quiet': True,
+        'outtmpl': queue_path,
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            print("Downloading audio now\n")
+            ydl.download([url])
+    except:
+        print("FALLBACK: youtube-dl does not support this URL, using Spotify (This is normal if spotify URL)")
+        q_path = os.path.abspath(os.path.realpath("Queue"))
+        system(f"spotdl -ff song{q_num} -f " + '"' + q_path + '"' + " -s " + url)
+
+    await ctx.send("Adding song " + str(q_num) + " to the queue")
+
+    print("Song added to queue\n")
+
+@client.command(pass_context=True, aliases=['skip'])
+async def next(ctx):
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_playing():
+        print("Playing next song")
+        voice.stop()
+        await ctx.send("PLaying next song")
+    else:
+        print("No music playing failed to stop")
+        await ctx.send("No music playing")
+
+
 
 player1 = ""
 player2 = ""
@@ -550,6 +761,20 @@ async def place_error(ctx,error):
     await ctx.send("Please make sure it is an integer")
 
 @client.command()
+async def test(ctx):
+  width = 9
+  height = 9
+  grid = ""
+  i = int(0)
+  for i in range(width):
+    grid+=(":white_large_square:")
+  for i in range(height):
+    await ctx.send(grid)
+  
+  
+
+
+@client.command()
 async def help(ctx):
   embed  = discord.Embed(title="Bot Help", colour=discord.Color.blue())
   embed.add_field(name="Forest", value = "Type >create {code} {starting time} {duration}. Dont include brackets", inline=False)
@@ -562,3 +787,4 @@ async def help(ctx):
   
 
 client.run("TOKEN")
+
